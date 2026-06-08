8c8dbd8d711eab218b564a73f795d6eb7d4fb502
tkatz123 <tylerkatz110@gmail.com>
2026-06-08 19:05:33 -0400

    Created Python environment, added job description data, created explore.py for initial quick data analysis

---
+++ ingestion/explore.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv('data/raw/AI_Engineer_Job_Data.csv')
+
+print("First 5 Rows:")
+print(df.head())
+
+print(" ")
+
+print("Rows, Columns")
+print(df.shape)
+
+print(df['Job_Description'].str.len().describe())
